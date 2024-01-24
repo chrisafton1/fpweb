1a39e88454e8920a4c72938071e27fa04e8f0b3e
BlazeGamesYT <bryceblazegaming@gmail.com>
2024-01-23 23:01:54 -0800

    uwu

--- main.js
+++ main.js
@@ -2,21 +2,16 @@ const tagline = document.getElementById("tagline");
 
 const options = [
     "Join the discord you silly goober :3",
-    "Join the discord you silly goober :3",
-    "Join the discord you silly goober :3",
     "JOIN IT",
     "Pwease join it :3",
     "Epic discord server for femboys :3",
-    "Epic discord server for femboys :3",
-    "Epic discord server for femboys :3",
-    "Epic discord server for femboys :3",
-    "Epic discord server for femboys :3",
     "Nya~~~~ joinnnn",
     "Join for free headpats",
-    "Join for free headpats",
-    "Join for free headpats",
-    "UWU"
+    "UWU",
+    "join cutie :3"
 ];
 
-const randomNumber = Math.floor(Math.random() * options.length);
-tagline.innerHTML = options[randomNumber];
+setInterval(() => {
+    let randomNumber = Math.floor(Math.random() * options.length);
+    tagline.innerHTML = options[randomNumber];
+}, 2000);
