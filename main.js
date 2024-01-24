6e93866c15237e69c9d932bbb8f5eefd8e75639d
BlazeGamesYT <bryceblazegaming@gmail.com>
2024-01-23 23:44:33 -0800

    Added seperate quotes.json file.

--- main.js
+++ main.js
@@ -1,17 +1,11 @@
+const response = await fetch("./quotes.json");
+const quotes = await response.json();
+
+console.log(quotes);
+
 const tagline = document.getElementById("tagline");
 
-const options = [
-    "Join the discord you silly goober :3",
-    "JOIN IT",
-    "Pwease join it :3",
-    "Epic discord server for femboys :3",
-    "Nya~~~~ joinnnn",
-    "Join for free headpats",
-    "UWU",
-    "join cutie :3"
-];
-
 setInterval(() => {
-    let randomNumber = Math.floor(Math.random() * options.length);
-    tagline.innerHTML = options[randomNumber];
+    let randomNumber = Math.floor(Math.random() * quotes.length);
+    tagline.innerHTML = quotes[randomNumber];
 }, 2000);
